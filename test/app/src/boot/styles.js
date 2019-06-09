f987f27910a2075b84971fdffa5e52039f2cf185
Tobias de Abreu Mesquita <tobias.mesquita@gmail.com>
2019-06-09 20:55:44 -0300

    button initial color

--- test/app/src/boot/styles.js
+++ test/app/src/boot/styles.js
@@ -1,5 +1,5 @@
 // import something here
-import { state as btnGroupState } from 'quasar-app-extension-qstyles/src/styles/btn-group'
+import { state as btnCommonState } from 'quasar-app-extension-qstyles/src/styles/btn-common'
 import { state as basicState } from 'quasar-app-extension-qstyles/src/styles/basic'
 import { state as fieldState } from 'quasar-app-extension-qstyles/src/styles/field'
 import { colors } from 'quasar'
@@ -8,7 +8,7 @@ import { colors } from 'quasar'
 export default async (context) => {
   colors.setBrand('primary', '#009688')
   colors.setBrand('secondary', '#673ab7')
-  btnGroupState.color = 'secondary'
+  btnCommonState.color = 'secondary'
   basicState.dark = true
   basicState.color = 'primary'
   fieldState.filled = false
