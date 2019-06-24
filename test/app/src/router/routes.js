33575b027db03972313ae495b9e38dba56193fbe
Tobias de Abreu Mesquita <tobias.mesquita@gmail.com>
2019-06-24 20:20:40 -0300

    redirect

--- test/app/src/router/routes.js
+++ test/app/src/router/routes.js
@@ -4,7 +4,7 @@ const routes = [
     path: '/',
     component: () => import('layouts/MyLayout.vue'),
     children: [
-      { path: '', component: () => import('pages/Index.vue') },
+      { path: '', component: () => import('pages/Index.vue'), redirect: '/export' },
       { path: 'colors', component: () => import('pages/Color.vue') },
       { path: 'export', component: () => import('pages/Export.vue') }
     ]
